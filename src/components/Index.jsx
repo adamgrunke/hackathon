7357d631d28e5270bc00ab0771788ee6bc7f1e6a
Anthony Zardis <anthonyzardis@gmail.com>
2019-07-30 08:22:40 -0700

    Index Page Header

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import IndexShow from './components/IndexShow';
+import PropTypes from 'prop-types';
 
 class Index extends React.Component {
-  
-
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
+  state = {
+    school: []
   }
+  
+  render() {
+    return this.props.schools.map( (school) => (
+      <>
+        {/* <IndexShow key={school.schoolList} 
+                  school={school} 
+                  
+        /> */}
+      </>
+    ))
+  } 
+}
+
+Index.propTypes = {
+  school: PropTypes.array.isRequired
 }
 
 export default Index;
---
+++ src/components/IndexShow.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class IndexShow extends React.Component {
+  // getStyle = () => {
+  //   return {
+  //     backgroundColor: '#f4f4f4',
+  //     padding: '10px',
+  //     borderBottom: '1px #ccc dotted',
+  //     textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+  //   }
+  // }
+
+  render() {
+    //const { schoolList, school } = this.props.school;
+    return (
+      // <div style={this.getStyle()}>
+      //   <p>
+      //     { schools }
+      //   </p>
+      // </div>
+    )
+  } 
+}
+
+// IndexShow.propTypes = {
+//   schools: PropTypes.object.isRequired
+// }
+export default IndexShow;
